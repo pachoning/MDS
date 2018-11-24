33c859e08af0a9c365ccbc932bfd223a78847bf2
cristianpachon <cc.pachon@gmail.com>
2018-11-24 20:01:29 +0100

    Script to load libraries

---
+++ TFM/tools/load_libraries.R
@@ -0,0 +1,61 @@
+if( require(cluster) == FALSE ){
+  install.packages('cluster')
+  library(cluster)
+}
+
+if( require(coenocliner) == FALSE ){
+  install.packages('coenocliner')
+  library(coenocliner)
+}
+
+
+if( require(future) == FALSE ){
+  install.packages('future')
+  library(future)
+}
+
+
+if( require(ggplot2) == FALSE ){
+  install.packages('ggplot2')
+  library(ggplot2)
+}
+
+if( require(ggrepel) == FALSE ){
+  install.packages('ggrepel')
+  library(ggrepel)
+}
+
+
+
+if( require(pracma) == FALSE ){
+  install.packages('pracma')
+  library(pracma)
+}
+
+if( require(R.utils) == FALSE ){
+  install.packages('R.utils')
+  library(R.utils)
+}
+
+if( require(rlist) == FALSE ){
+  install.packages('rlist')
+  library(rlist)
+}
+
+if( require(smacof) == FALSE ){
+  install.packages('smacof')
+  library(smacof)
+}
+
+
+
+if( require(tidyverse) == FALSE ){
+  install.packages('tidyverse')
+  library(tidyverse)
+}
+
+
+if( require(vegan) == FALSE ){
+  install.packages('vegan')
+  library(vegan)
+}
