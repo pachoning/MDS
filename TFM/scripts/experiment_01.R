53b7a57af8a0e31a8b04a416bcae5c09a1733ff9
cristianpachon <cc.pachon@gmail.com>
2020-12-25 18:02:42 +0100

    Change experiment_01 script format

--- TFM/scripts/experiment_01.R
+++ TFM/scripts/experiment_01.R
@@ -1,6 +1,6 @@
 source("tools/simulator.R")
 
-data_folder_name = 'experiment_1'
+results_folder_name = 'experiment_01'
 sample_size = c(1000, 2000, 5000)
 n_cols = c(10, 100)
 
@@ -9,12 +9,11 @@ distribution_parameters = list(list(sd=c(NA)), list(sd=c(15)), list(sd=c(15,15))
 
 scenarios = list(sample_size=sample_size, n_cols=n_cols, distribution_parameters=distribution_parameters)
 
-
 get_simulations(
   scenarios=scenarios,
-  path=file.path(getwd(), 'data', data_folder_name),
+  path=file.path(getwd(), 'data', results_folder_name),
   mds_methods_vector=c(divide_conquer_mds, fast_mds, gower_interpolation_mds),
-  n_simulations=1,
+  n_simulations=100,
   overwrite_simulations=FALSE,
   n_sampling_points=NA,
   largest_matrix_efficient_mds=200,
