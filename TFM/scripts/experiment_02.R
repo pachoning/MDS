171961835496c9ea13584c618947c874bd3e8cf3
cristianpachon <cc.pachon@gmail.com>
2020-12-28 15:00:23 +0100

    Add experiment 2 and 3 scripts

---
+++ TFM/scripts/experiment_02.R
@@ -0,0 +1,23 @@
+source("tools/simulator.R")
+
+results_folder_name = 'experiment_02'
+sample_size = c(10000, 100000)
+n_cols = c(10, 100)
+
+distribution_parameters = list(list(sd=c(NA)), list(sd=c(15)), list(sd=c(15,15)), list(sd=c(15,10)), 
+                               list(sd=c(15, 10, 5)), list(sd=c(15, 10, 5, 5)))
+
+scenarios = list(sample_size=sample_size, n_cols=n_cols, distribution_parameters=distribution_parameters)
+
+get_simulations(
+  scenarios=scenarios,
+  path=file.path(getwd(), 'data', results_folder_name),
+  mds_methods_vector=c(divide_conquer_mds, fast_mds, gower_interpolation_mds),
+  n_simulations=100,
+  overwrite_simulations=FALSE,
+  n_sampling_points=NA,
+  largest_matrix_efficient_mds=200,
+  largest_matrix_efficient_procrustes=5000,
+  num_mds_dimesions=NA,
+  verbose=TRUE
+)
