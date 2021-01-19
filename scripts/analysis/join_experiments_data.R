a3286d748d67dc85a508c95a48c2f5d7ac358ad2
cristianpachon <cc.pachon@gmail.com>
2021-01-19 17:19:48 +0100

    Build full dataset for eigenvalues

--- scripts/analysis/join_experiments_data.R
+++ scripts/analysis/join_experiments_data.R
@@ -13,25 +13,29 @@ total_experiments_to_retrieve = length(experiments_to_retrieve)
 df_scenarios_full = c()
 df_time_full = c()
 df_correlation_full = c()
+df_eigenvalues_full = c()
 
 i_experiment = 1
-while(i_experiment <= total_experiments_to_retrieve){
+while (i_experiment <= total_experiments_to_retrieve) {
   current_experiment = file.path(experiments_folder, experiments_to_retrieve[i_experiment])
   experiment_files = list.files(current_experiment)
-  for(file in experiment_files){
-    if(stringi::stri_detect_regex(str=file, pattern="df_scenarios.RData")){
+  for (file in experiment_files) {
+    if (stringi::stri_detect_regex(str=file, pattern="df_scenarios.RData")) {
       load(file.path(current_experiment, file))
       df_scenarios_full = rbind(df_scenarios_full, df_scenarios)
       rm(df_scenarios)
-    }else if(stringi::stri_detect_regex(str=file, pattern="df_time.RData")){
+    } else if (stringi::stri_detect_regex(str=file, pattern="df_time.RData")) {
       load(file.path(current_experiment, file))
       df_time_full = rbind(df_time_full, df_time)
       rm(df_time)
-      
-    }else if(stringi::stri_detect_regex(str=file, pattern="df_correlation.RData")){
+    } else if(stringi::stri_detect_regex(str=file, pattern="df_correlation.RData")) {
       load(file.path(current_experiment, file))
       df_correlation_full = rbind(df_correlation_full, df_correlation)
       rm(df_correlation)
+    } else if(stringi::stri_detect_regex(str=file, pattern="df_eigenvalue.RData")) {
+      load(file.path(current_experiment, file))
+      df_eigenvalues_full = rbind(df_eigenvalues_full, df_eigenvalue)
+      rm(df_eigenvalue)
     }
   }
   
@@ -51,12 +55,10 @@ df_scenarios_full = df_scenarios_full %>%
 
 # Validations
 setdiff(df_scenarios_full$id, unique(df_time_full$scenario_id))
-setdiff(unique(df_time_full$scenario_id), df_scenarios_full$id)
 setdiff(df_scenarios_full$id, unique(df_correlation_full$scenario_id))
-setdiff(unique(df_correlation_full$scenario_id), df_scenarios_full$id)
-setdiff(unique(df_time_full$scenario_id), unique(df_correlation_full$scenario_id))
-setdiff(unique(df_correlation_full$scenario_id), unique(df_time_full$scenario_id))
+setdiff(df_scenarios_full$id, unique(df_eigenvalues_full$scenario_id))
 
 save(df_scenarios_full, file=file.path(data_folder, "df_scenarios_full.RData"))
 save(df_time_full, file=file.path(data_folder, "df_time_full.RData"))
 save(df_correlation_full, file=file.path(data_folder, "df_correlation_full.RData"))
+save(df_eigenvalues_full, file=file.path(data_folder, "df_eigenvalues_full.RData"))
