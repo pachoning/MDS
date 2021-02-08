4f1e55ca2aff74138afe3e88ba61ad5e0a15de5a
cristianpachon <cc.pachon@gmail.com>
2021-02-08 13:05:26 +0100

    Add validations for new method based on Gower Interpolation

--- scripts/validations/validate_methods.R
+++ scripts/validations/validate_methods.R
@@ -3,6 +3,7 @@ source("tools/fast_mds.R")
 source("tools/fast_mds_candidate_1.R")
 source("tools/divide_conquer_mds.R")
 source("tools/gower_interpolation_mds.R")
+source("tools/gower_interpolation_mds_candidate_1.R")
 source("tools/procrustes.R")
 source("tools/load_libraries.R")
 
@@ -31,6 +32,7 @@ cor(divide_proc[,1], x[, 1])
 sd(divide_results$points[, 1])
 sqrt(divide_results$eigen)
 
+########################################################################################################################
 
 ##### Results for fast mds
 fast_results <- fast_mds(x = x,l = l, s = s, k = k, dist_fn = stats::dist)
@@ -44,9 +46,10 @@ cor(fast_proc[,1], x[, 1])
 sd(fast_results$points[, 1])
 sqrt(fast_cand_1$eigen)
 
+########################################################################################################################
 
 ##### Results for candidate_1 of mds
-fast_cand_1 <- fast_mds_candidate_1(x = x, l =l, s = s, k = k, dist_fn = stats::dist)
+fast_cand_1 <- fast_mds_candidate_1(x = x, l = l, s = s, k = k, dist_fn = stats::dist)
 fast_cand_1_proc <- perform_procrustes(x = fast_cand_1$points, target = x, matrix_to_transform = fast_cand_1$points, 
                                 translation = FALSE, dilation = FALSE)
 # Correlation of principal coordinates
@@ -56,7 +59,7 @@ cor(fast_cand_1_proc[,1], x[, 1])
 sd(fast_cand_1$points[, 1])
 sqrt(fast_results$eigen)
 
-
+########################################################################################################################
 
 ##### Results for Gower mds
 gower_results <- gower_interpolation_mds(x = x, l = l, k = k, dist_fn = stats::dist)
@@ -70,3 +73,15 @@ cor(gower_proc[,1], x[, 1])
 sd(gower_results$points[, 1])
 sqrt(gower_results$eigen)
 
+########################################################################################################################
+
+##### Results for candidate_1 of Gower mds
+gower_cand_1 <- gower_interpolation_mds_candidate_1(x = x, l = l, k = k, dist_fn = stats::dist)
+gower_cand_1_proc <- perform_procrustes(x = gower_cand_1$points, target = x, matrix_to_transform = gower_cand_1$points, 
+                                       translation = FALSE, dilation = FALSE)
+# Correlation of principal coordinates
+cor(gower_cand_1_proc[,1], x[, 1])
+
+# Eigenvalues
+sd(gower_cand_1$points[, 1])
+sqrt(gower_cand_1$eigen)
