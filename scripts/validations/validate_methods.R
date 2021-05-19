c7a149b07c2fd402df68e983b685f536a078636f
cristianpachon <cc.pachon@gmail.com>
2021-05-19 12:48:36 +0200

    Update script that validates methods

--- scripts/validations/validate_methods.R
+++ scripts/validations/validate_methods.R
@@ -1,29 +1,36 @@
-source("tools/classical_mds.R")
-source("tools/fast_mds.R")
-source("tools/fast_mds_candidate_1.R")
-source("tools/divide_conquer_mds.R")
-source("tools/gower_interpolation_mds.R")
-source("tools/gower_interpolation_mds_candidate_1.R")
-source("tools/procrustes.R")
 source("tools/load_libraries.R")
+source("tools/procrustes.R")
+source("final_methods/classical_mds.R")
+source("final_methods/fast_mds.R")
+source("final_methods/divide_conquer_mds.R")
+source("tools/gower_interpolation_mds.R")
+
 
 # Generate data
-n_rows <- 10000
+n_rows <- 1000
 var_vector <- c(5, 5, 1)
+mean_vector <- c(0, 0, 0)
 n_cols <- length(var_vector)
-x <- matrix(rnorm(n_rows*n_cols), nrow = n_rows) %*% diag(var_vector)
+x <- mapply(rnorm, n = n_rows, mean_vector) %*% diag(var_vector)
 dim(x)
 var(x)
 cor(x)
 
 s <- 2*n_cols
+tie <- s
 k <- n_cols
-l <- 100
+l <- 200
 
+########################################################################################################################
 ##### Results for divide and conquer mds
-divide_results <- divide_conquer_mds(x = x,l = l, tie = s, k = k, dist_fn = stats::dist)
+divide_results <- divide_conquer_mds(x = x,l = l, tie = tie, k = k, dist_fn = stats::dist)
 divide_proc <- perform_procrustes(x = divide_results$points, target = x, matrix_to_transform = divide_results$points, 
                                  translation = FALSE, dilation = FALSE)
+
+# Checking it is a MDS solution
+cov(divide_results$points)
+cor(divide_results$points)
+apply(divide_results$points, 2, mean)
 
 # Correlation of principal coordinates
 cor(divide_proc[,1], x[, 1])
@@ -32,39 +39,44 @@ cor(divide_proc[,1], x[, 1])
 sd(divide_results$points[, 1])
 sqrt(divide_results$eigen)
 
-########################################################################################################################
+# Cheching distance is preserved
+dist(x[1:5, ])
+dist(divide_results$points[1:5, ])
 
+
+########################################################################################################################
 ##### Results for fast mds
 fast_results <- fast_mds(x = x,l = l, s = s, k = k, dist_fn = stats::dist)
 fast_proc <- perform_procrustes(x = fast_results$points, target = x, matrix_to_transform = fast_results$points, 
                                  translation = FALSE, dilation = FALSE)
+
+# Checking it is a MDS solution
+cov(fast_results$points)
+cor(fast_results$points)
+apply(fast_results$points, 2, mean)
 
 # Correlation of principal coordinates
 cor(fast_proc[,1], x[, 1])
 
 # Eigenvalues
 sd(fast_results$points[, 1])
-sqrt(fast_cand_1$eigen)
-
-########################################################################################################################
-
-##### Results for candidate_1 of mds
-fast_cand_1 <- fast_mds_candidate_1(x = x, l = l, s = s, k = k, dist_fn = stats::dist)
-fast_cand_1_proc <- perform_procrustes(x = fast_cand_1$points, target = x, matrix_to_transform = fast_cand_1$points, 
-                                translation = FALSE, dilation = FALSE)
-# Correlation of principal coordinates
-cor(fast_cand_1_proc[,1], x[, 1])
-
-# Eigenvalues
-sd(fast_cand_1$points[, 1])
 sqrt(fast_results$eigen)
 
-########################################################################################################################
+# Cheching distance is preserved
+dist(x[1:5, ])
+dist(fast_results$points[1:5, ])
 
+########################################################################################################################
 ##### Results for Gower mds
 gower_results <- gower_interpolation_mds(x = x, l = l, k = k, dist_fn = stats::dist)
 gower_proc <- perform_procrustes(x = gower_results$points, target = x, matrix_to_transform = gower_results$points, 
                                 translation = FALSE, dilation = FALSE)
+
+# Checking it is a MDS solution
+cov(gower_results$points)
+cor(gower_results$points)
+apply(gower_results$points, 2, mean)
+
 
 # Correlation of principal coordinates
 cor(gower_proc[,1], x[, 1])
@@ -73,15 +85,6 @@ cor(gower_proc[,1], x[, 1])
 sd(gower_results$points[, 1])
 sqrt(gower_results$eigen)
 
-########################################################################################################################
-
-##### Results for candidate_1 of Gower mds
-gower_cand_1 <- gower_interpolation_mds_candidate_1(x = x, l = l, k = k, dist_fn = stats::dist)
-gower_cand_1_proc <- perform_procrustes(x = gower_cand_1$points, target = x, matrix_to_transform = gower_cand_1$points, 
-                                       translation = FALSE, dilation = FALSE)
-# Correlation of principal coordinates
-cor(gower_cand_1_proc[,1], x[, 1])
-
-# Eigenvalues
-sd(gower_cand_1$points[, 1])
-sqrt(gower_cand_1$eigen)
+# Cheching distance is preserved
+dist(x[1:5, ])
+dist(gower_results$points[1:5, ])
