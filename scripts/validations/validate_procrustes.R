aa331bd22557bcb8be5ee6c3f5ec4843112213d1
cristianpachon <cc.pachon@gmail.com>
2021-06-11 12:13:20 +0200

    change format of validations scripts

--- scripts/validations/validate_procrustes.R
+++ scripts/validations/validate_procrustes.R
@@ -8,8 +8,7 @@ x = matrix(rnorm(n_rows*n_cols, sd = 10), nrow = n_rows)
 rot_mat = matrix(data=c(cos(45), -sin(45), sin(45), cos(45)), ncol = n_cols, nrow = n_cols)
 y = x %*% rot_mat
 
-proc = perform_procrustes(x=x, target=y, matrix_to_transform=x, 
-                          translation=FALSE, dilation=FALSE)
+proc = perform_procrustes(x = x, target = y, matrix_to_transform = x, translation = FALSE, dilation = FALSE)
 
 head(proc)
 head(y)
