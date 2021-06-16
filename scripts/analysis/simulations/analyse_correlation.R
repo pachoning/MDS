59bd6c772490fa556ce13f6f4a025a501d58a9f8
cristianpachon <cc.pachon@gmail.com>
2021-06-16 15:52:34 +0200

    Modify scripts for plots in the analysis of correlation

--- scripts/analysis/simulations/analyse_correlation.R
+++ scripts/analysis/simulations/analyse_correlation.R
@@ -1,3 +1,4 @@
+options(digits = 12)   
 library(tidyverse)
 library(ggplot2)
 
@@ -5,6 +6,7 @@ library(ggplot2)
 data_path = file.path(getwd(), 'data')
 load(file.path(data_path, "df_scenarios_full.RData"))
 load(file.path(data_path, "df_correlation_full.RData"))
+load(file.path(data_path, "df_conversion.RData"))
 
 # Manipulate data ----
 scenario_identifier = c("sample_size", "n_cols", "n_main_dimensions", "var_main")
@@ -14,119 +16,68 @@ scenarios_with_main_dimesions = df_scenarios_full %>% filter(n_main_dimensions >
 
 # Join scenarios and correlation
 df_join_scenarios_correlation = scenarios_with_main_dimesions %>% 
-  select_at(c("id", scenario_identifier))   %>% 
+  select_at(c("id", scenario_identifier)) %>% 
   left_join(
     df_correlation_full %>% select(-n_main_dimensions),
     by = c("id" = "scenario_id")
   ) %>% unnest(correlation_vector) %>% 
   rename(correlation = correlation_vector) %>% 
   group_by(id, method_name, num_sim) %>% 
-  mutate(
-    dim = paste0("dim_", sprintf("%02d", 1:n())),
-    log_1_correlation = log(1-correlation)
-  )
+  mutate(dim = paste0("dim_", sprintf("%02d", 1:n()))) %>% 
+  left_join(df_conversion)
+
+# Validations ----
+# Unique combinations
+df_join_scenarios_correlation %>% 
+  group_by(sample_size, n_cols, n_main_dimensions, algorithm, dim) %>% 
+  summarise(total = n()) %>% 
+  filter(total > 100)
+
 
 # Analyse data  ----
+# Table of correlations
+quant = c(0.025, 1-0.025)
+quant_char = c(paste0("q_", as.character(quant)), "mean")
+df_join_scenarios_correlation %>% 
+  group_by(algorithm) %>% 
+  summarise(
+    statistic = quant_char,
+    value = c(quantile(correlation, quant), mean(correlation))) %>% 
+  pivot_wider(
+    names_from = statistic,
+    values_from = value) %>% 
+  mutate_if(
+    is.numeric,
+    function(x) formatC(x, format = "e", digits = 5)
+  )
+
 # Plots
-total_scenarios = nrow(scenarios_with_main_dimesions)
-i = 1
-
-while(i <= total_scenarios){
-  current_scenario = scenarios_with_main_dimesions[i, ]
-  scenario_id = current_scenario$id
-  n_main_dimensions = current_scenario$n_main_dimensions
-  
-  correlation_data = df_join_scenarios_correlation[df_join_scenarios_correlation$id == scenario_id, ]
-  
-  name_title = paste0("sample_size: ", current_scenario$sample_size, "; n_cols: ", current_scenario$n_cols, 
-                      "; n_main_dim: ", current_scenario$n_main_dimensions, "; var_main: ", current_scenario$var_main)
-  
-  p = correlation_data %>% 
-    ggplot(aes(x = method_name, y = correlation)) + 
-    geom_boxplot() + 
-    facet_wrap(. ~ dim, ncol = n_main_dimensions) +
-    ggtitle(name_title) +
-    theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-  
-  png(file.path("plots", "correlation", "plot_correlation", paste0(name_title, ".png")))
-  print(p)
-  dev.off()
-  
-  p = correlation_data %>% 
-    ggplot(aes(x = method_name, y = log_1_correlation, fill = method_name)) + 
-    geom_boxplot() + 
-    facet_wrap(. ~ dim, ncol = n_main_dimensions) +
-    ggtitle(name_title) +
-    theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
-          legend.position="none")
-  
-  png(file.path("plots", "correlation", "plot_log_correlation", paste0(name_title, ".png")))
-  print(p)
-  dev.off()
-  
-  i = i + 1
-}
-
-# Anova analysis
-# Measuring just the effect of the method
-linear_model_method = lm(correlation ~ method_name, data = df_join_scenarios_correlation)
-anova(linear_model_method)
-summary(linear_model_method)
-
-# Add sample size and interaction between method_name and sample size
-linear_model_method_sample_size = df_join_scenarios_correlation %>% mutate(sample_size = as.factor(sample_size))%>% 
-  lm(correlation ~ method_name + sample_size + method_name:sample_size, data = .)
-anova(linear_model_method_sample_size)
-summary(linear_model_method_sample_size)
-
-# Plot of the correlation of the first dimension
+# Plot of the correlation
 df_join_scenarios_correlation %>% 
-  group_by(method_name, sample_size, dim) %>%
+  group_by(algorithm, sample_size, dim) %>%
   summarise(mean_correlation = mean(correlation)) %>%
   mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mean_correlation, color = method_name, group = method_name)) +
+  ggplot(aes(x = sample_size, y = mean_correlation, color = algorithm, group = algorithm)) +
   geom_point() +
   facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/correlation_all.png", 
-         dpi=300, dev='png', height=8, width=10, units="in")
-
-df_join_scenarios_correlation %>% 
-  filter(id %in% scenarios_with_main_dimesions$id, sample_size<10^5) %>% 
-  group_by(method_name, sample_size, dim) %>%
-  summarise(mean_correlation = mean(correlation)) %>%
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mean_correlation, color = method_name, group = method_name)) +
-  geom_point() +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave(" ~/Documents/phd/papers/mds_for_big_data/images/correlation_small.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
-
-df_join_scenarios_correlation %>% 
-  filter(id %in% scenarios_with_main_dimesions$id, sample_size>=10^5) %>% 
-  group_by(method_name, sample_size, dim) %>%
-  summarise(mean_correlation = mean(correlation)) %>%
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mean_correlation, color = method_name, group = method_name)) +
-  geom_point() +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/correlation_big.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
-
-
+  theme(panel.spacing.y = unit(0.5, "lines"), legend.position="bottom") +
+  xlab("Sample Size") + 
+  ylab("Mean Correlation") +
+  ggsave(file.path(getwd(), "images", "correlation_all.png"),
+         dpi = 300, dev = 'png', height = 18, width = 22, units = "cm")
+  
 # Particular cases
 df_join_scenarios_correlation %>% 
   filter(sample_size == 10^6, n_main_dimensions == 10, n_cols == 100) %>% 
-  ggplot(aes(x = method_name, y = correlation, fill = method_name)) + 
+  ggplot(aes(x = algorithm, y = correlation, fill = algorithm)) + 
   geom_boxplot() + 
   facet_wrap(. ~ dim, ncol = 5) +
   theme(
-    panel.spacing.y=unit(0.5, "lines"), 
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
-  ) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/correlation_1000000_100_10.png", 
-         dpi=300, dev='png', height=8, width=10, units="in")
+    panel.spacing.y = unit(0.5, "lines"), 
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = "bottom") +
+  ylab("Correlation coefficient") +
+  ggsave(file.path(getwd(), "images", "correlation_1000000_100_10.png"),
+         dpi = 300, dev = 'png', height = 18, width = 22, units = "cm")
