fb882d4f52d0a33d84d445c316acb0f502575094
cristianpachon <cc.pachon@gmail.com>
2021-06-16 18:24:26 +0200

    Delete unnecessary code from script that analyses the eigenvalues and the variance

--- scripts/analysis/simulations/analyse_eigenvalues.R
+++ scripts/analysis/simulations/analyse_eigenvalues.R
@@ -5,6 +5,7 @@ library(ggplot2)
 data_path = file.path(getwd(), 'data')
 load(file.path(data_path, "df_scenarios_full.RData"))
 load(file.path(data_path, "df_eigenvalues_full.RData"))
+load(file.path(data_path, "df_conversion.RData"))
 
 # Manipulate data ----
 scenario_identifier = c("sample_size", "n_cols", "n_main_dimensions", "var_main")
@@ -39,7 +40,7 @@ while (i <= total_scenarios) {
   var_sim$method_name = eigen_data$method_name
   
   eigenvalues_sim = map2(.x = eigen_data$eigenvalue_vector, .y = eigen_data$n_main_dimensions, .f = ~ .x[1:.y]) %>% 
-    unlist() %>% matrix(., nrow = length(eigen_data$var), byrow = TRUE) %>% sqrt() %>% as.data.frame() %>%
+    unlist() %>% matrix(., nrow = length(eigen_data$var), byrow = TRUE) %>% as.data.frame() %>%
     set_names(nm = paste0("dim_", sprintf("%02d", 1:n_main_dimension_scenario)))
   
   eigenvalues_sim$scenario_id = scenario_id
@@ -65,142 +66,61 @@ df_eigenvalues_long = df_eigenvalues_flat %>%
   pivot_longer(cols = starts_with("dim"), names_to = "dim", values_to = "eigenvalue") %>% 
   filter(!is.na(eigenvalue))
 
-
 # Enrich scenarios with eigenvalue information
 eigenvalues_information = df_join_scenarios_eigenvalues %>% 
   select_at(c("id", scenario_identifier, "num_sim", "method_name")) %>% 
   left_join(df_var_flat_long, by = c("id" = "scenario_id", "num_sim" = "num_sim", "method_name" = "method_name")) %>% 
   left_join(df_eigenvalues_long, by = c("id" = "scenario_id", "num_sim" = "num_sim", "method_name" = "method_name", "dim" = "dim")) %>% 
-  mutate(
-    error = eigenvalue - var
-  )
+  left_join(df_conversion) %>% 
+  mutate(error = eigenvalue - var)
 
 # Analyse data  ----
-# Boxplot for the error
-i = 1
-while (i<=total_scenarios) {
-  current_scenario = scenarios_with_main_dimensions[i, ]
-  scenario_id = current_scenario$id
-  n_main_dimensions = current_scenario$n_main_dimensions
-  eigenvalues_information_i = eigenvalues_information %>% filter(id == scenario_id)
-  
-  name_title = paste0("sample_size: ", current_scenario$sample_size, "; n_cols: ", current_scenario$n_cols, 
-                      "; n_main_dim: ", n_main_dimensions, "; var_main: ", current_scenario$var_main)
-  
-  p = eigenvalues_information_i %>% 
-    ggplot(aes(x = method_name, y = error, fill = method_name)) + geom_boxplot() + facet_wrap(. ~ dim, ncol = n_main_dimensions) +
-    ggtitle(name_title) +
-    theme(plot.title = element_text(hjust = 0.5), axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position="none")
-  
-  png(file.path("plots", "eigenvalues", paste0(name_title, ".png")))
-  print(p)
-  dev.off()
-  i = i + 1
-}
-
-
 # Bias
 eigenvalues_information %>%
-  group_by(sample_size, method_name, dim) %>%
-  summarise(
-    bias = mean(eigenvalue) - max(var)
-  ) %>% 
+  group_by(sample_size, algorithm, dim) %>%
+  summarise(bias = mean(error)) %>% 
   mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = bias, group = method_name, color = method_name)) +
+  ggplot(aes(x = sample_size, y = bias, group = algorithm, color = algorithm)) +
   geom_point() +
   geom_hline(yintercept = 0, linetype = "dashed") +
   facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/bias_all.png", 
-         dpi=300, dev='png', height=8, width=10, units="in")
-
-eigenvalues_information %>%
-  filter(sample_size < 10^5) %>%
-  group_by(sample_size, method_name, dim) %>%
-  summarise(
-    bias = mean(eigenvalue) - max(var)
-  ) %>% 
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = bias, group = method_name, color = method_name)) +
-  geom_point() +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/bias_small.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
-
-eigenvalues_information %>%
-  filter(sample_size >= 10^5) %>%
-  group_by(sample_size, method_name, dim) %>%
-  summarise(
-    bias = mean(eigenvalue) - max(var)
-  ) %>% 
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = bias, group = method_name, color = method_name)) +
-  geom_point() +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/bias_big.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
-
+  theme(panel.spacing.y=unit(0.5, "lines"), legend.position="bottom") +
+  xlab("Sample Size") + 
+  ylab("Bias") +
+  ggsave(file.path(getwd(), "images", "bias_all.png"),
+         dpi = 300, dev = 'png', height = 18, width = 22, units = "cm")
+ 
 # MSE
 eigenvalues_information %>% 
-  group_by(sample_size, method_name, dim) %>% 
+  group_by(sample_size, algorithm, dim) %>% 
   summarise(
     mse = mean(error^2),
     rmse = sqrt(mse)
   ) %>% 
   mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mse, group = method_name, color = method_name)) +
+  ggplot(aes(x = sample_size, y = mse, group = algorithm, color = algorithm)) +
   geom_point() +
   facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/mse_all.png", 
-         dpi=300, dev='png', height=8, width=10, units="in")
-
-eigenvalues_information %>% 
-  filter(sample_size < 10^5) %>% 
-  group_by(sample_size, method_name, dim) %>% 
-  summarise(
-    mse = mean(error^2),
-    rmse = sqrt(mse)
-  ) %>% 
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mse, group = method_name, color = method_name)) +
-  geom_point() +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/mse_small.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
-
-eigenvalues_information %>% 
-  filter(sample_size >= 10^5) %>% 
-  group_by(sample_size, method_name, dim) %>% 
-  summarise(
-    mse = mean(error^2),
-    rmse = sqrt(mse)
-  ) %>% 
-  mutate(sample_size = as.factor(sample_size)) %>% 
-  ggplot(aes(x = sample_size, y = mse, group = method_name, color = method_name)) +
-  geom_point() +
-  facet_wrap( ~ dim, ncol = 2) +
-  theme(panel.spacing.y=unit(0.5, "lines")) +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/mse_big.png", 
-         dpi=300, dev='png', height=8, width=6.5, units="in")
+  theme(panel.spacing.y=unit(0.5, "lines"), legend.position="bottom") +
+  xlab("Sample Size") + 
+  ylab("MSE") +
+  ggsave(file.path(getwd(), "images", "mse_all.png"),
+         dpi = 300, dev = 'png', height = 18, width = 22, units = "cm")
 
 # Error for a particular scenario
 eigenvalues_information %>% 
   filter(sample_size == 10^6, n_main_dimensions == 10, n_cols == 100) %>% 
-  ggplot(aes(x = method_name, y = error, fill = method_name)) +
+  ggplot(aes(x = algorithm, y = error, fill = algorithm)) +
   geom_boxplot() + 
   facet_wrap( ~ dim, ncol = 5) +
   theme(
     panel.spacing.y=unit(0.5, "lines"), 
     axis.title.x=element_blank(),
     axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
+    axis.ticks.x=element_blank(),
+    legend.position="bottom"
   ) +
+  ylab("Error") +
   geom_hline(yintercept = 0, linetype = "dashed") +
-  ggsave("~/Documents/phd/papers/mds_for_big_data/images/error_1000000_100_10.png", 
-         dpi=300, dev='png', height=8, width=10, units="in")
+  ggsave(file.path(getwd(), "images", "error_1000000_100_10.png"),
+         dpi = 300, dev = 'png', height = 18, width = 22, units = "cm")
