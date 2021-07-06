d55d244ff4da15d530294f4f74963808c1b15a8b
cristianpachon <cc.pachon@gmail.com>
2021-07-06 11:55:15 +0200

    Add new methods to validate

--- scripts/validations/validate_methods.R
+++ scripts/validations/validate_methods.R
@@ -4,6 +4,8 @@ source("final_methods/classical_mds.R")
 
 source("final_methods/divide_conquer_mds.R")
 source("final_methods/divide_conquer_small_mds.R")
+source("final_methods/divide_conquer_stepwise_mds.R")
+
 source("final_methods/fast_mds.R")
 
 source("final_methods/gower_interpolation_mds.R")
@@ -13,24 +15,25 @@ source("final_methods/gower_interpolation_dist_mds.R")
 source("final_methods/gower_interpolation_big_mds.R")
 
 # Generate data
-n_rows <- 1000
-var_vector <- c(5, 5, 1)
-mean_vector <- c(0, 0, 0)
-n_cols <- length(var_vector)
+n_rows <- 10^4
+n_cols <- 10
+n_dominant_dimension <- 5
+var_vector <- c(rep(15, n_dominant_dimension), rep(1, n_cols - n_dominant_dimension))
+mean_vector <- rep(0, n_cols)
 x <- mapply(rnorm, n = n_rows, mean_vector) %*% diag(sqrt(var_vector))
 dim(x)
-var(x)
-cor(x)
+#var(x)
+#cor(x)
 
 s <- 2*n_cols
 tie <- s
 k <- n_cols
-l <- 200
+l <- 1500
 dist_fn = stats::dist
 
 ########################################################################################################################
 ##### Results 
-algorithm <- gower_interpolation_big_mds
+algorithm <- divide_conquer_stepwise_mds
 results <- algorithm(x = x,l = l, tie = tie, k = k, dist_fn = dist_fn, s = s)
 procrustes <- perform_procrustes(x = results$points,
                                  target = x, 
