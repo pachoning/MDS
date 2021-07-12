a65bce88783e51aaf8a627465df7d56fda1ec5fa
cristianpachon <cc.pachon@gmail.com>
2021-07-12 16:01:42 +0200

    Add extra validations

--- scripts/validations/validate_methods.R
+++ scripts/validations/validate_methods.R
@@ -6,8 +6,8 @@ source("final_methods/gower_interpolation_mds.R")
 source("final_methods/fast_mds.R")
 
 # Generate data
-n_rows <- 10^4
-n_cols <- 10
+n_rows <- 10^5
+n_cols <- 100
 n_dominant_dimension <- 5
 var_vector <- c(rep(15, n_dominant_dimension), rep(1, n_cols - n_dominant_dimension))
 mean_vector <- rep(0, n_cols)
@@ -23,10 +23,21 @@ l <- 1500
 n_row_partition <- 1
 dist_fn <-  stats::dist
 
-########################################################################################################################
+##############################################################################################################
 ##### Results 
 algorithm <- gower_interpolation_mds
-results <- algorithm(x = x,l = l, tie = tie, k = k, dist_fn = dist_fn, s = s, n_row_partition = n_row_partition)
+all_n_row_partition <- c(1, seq(from = 100, to = 1500, by = 100), (n_rows - l))
+
+for (n_row_partition in all_n_row_partition) {
+  ini_time <- proc.time()
+  results <- algorithm(x = x, l = l, tie = tie, k = k, dist_fn = dist_fn, s = s, n_row_partition = n_row_partition)
+  final_time <- proc.time()
+  elapsed_time <- final_time - ini_time
+  elapsed_time <- elapsed_time[3]
+  msg <- paste0("For value: ", n_row_partition, " the time has been: ", elapsed_time)
+  message(msg)
+}
+
 procrustes <- perform_procrustes(x = results$points,
                                  target = x, 
                                  matrix_to_transform = results$points,
