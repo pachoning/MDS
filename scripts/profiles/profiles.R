5fdee0d25834c56a394f8875784e3882a350d8a0
cristianpachon <cc.pachon@gmail.com>
2021-07-12 16:01:25 +0200

    Change profile validations

--- scripts/profiles/profiles.R
+++ scripts/profiles/profiles.R
@@ -1,28 +1,22 @@
 library(profvis)
 source("final_methods/divide_conquer_mds.R")
-source("final_methods/divide_conquer_stepwise_mds.R")
 source("final_methods/fast_mds.R")
-#source("final_methods/gower_interpolation_mds.R")
-#source("final_methods/gower_interpolation_rowwise_mds.R")
+source("final_methods/gower_interpolation_mds.R")
 
-iterations = 3
-k = 5
-s = 2*k
-#n = s^(iterations + 2)
-n = 10^4
-cols = 100
-x = matrix(data = rnorm(n*cols), ncol = cols)
-#l = (n*s^iterations)^(1/(iterations + 1))
-l = 1500
-tie = s
-dist_fn = stats::dist
+iterations <- 3
+k <- 5
+s <- 2*k
+#n <- s^(iterations + 2)
+n <- 10^5
+cols <- 100
+x <- matrix(data = rnorm(n*cols), ncol = cols)
+#l <- (n*s^iterations)^(1/(iterations + 1))
+l <- 1500
+tie <- s
+dist_fn <- stats::dist
 
 profvis({
   divide_conquer_mds(x, l, tie, k)
-})
-
-profvis({
-  divide_conquer_stepwise_mds(x, l, tie, k)
 })
 
 
