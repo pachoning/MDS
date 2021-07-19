4735fa91070bd15f6aecc1e08685aefb0d235828
cristianpachon <cc.pachon@gmail.com>
2021-07-19 14:49:42 +0200

    Modify experiment 4

--- scripts/experiments/experiment_04.R
+++ scripts/experiments/experiment_04.R
@@ -6,13 +6,13 @@ source("final_methods/fast_mds.R")
 results_folder_name <- "experiment_04"
 experiment_label <- "experiment_10^5"
 
-sample_size = c(10^5)
-n_cols = c(10, 100)
+sample_size <- c(10^5)
+n_cols <- c(10, 100)
 
-distribution_parameters = list(list(var = 15), list(var = rep(15, 2)), list(var = rep(15, 3)), 
-                               list(var = rep(15, 4)), list(var = rep(15, 5)), list(var = rep(15, 6)), 
-                               list(var = rep(15, 7)), list(var = rep(15, 8)), list(var = rep(15, 9)), 
-                               list(var = rep(15, 10)))
+distribution_parameters <- list(list(var = 15), list(var = rep(15, 2)), list(var = rep(15, 3)), 
+                                list(var = rep(15, 4)), list(var = rep(15, 5)), list(var = rep(15, 6)), 
+                                list(var = rep(15, 7)), list(var = rep(15, 8)), list(var = rep(15, 9)), 
+                                list(var = rep(15, 10)))
 
 scenarios <- list(sample_size = sample_size, n_cols = n_cols, distribution_parameters = distribution_parameters)
 algorithms <- list(divide = divide_conquer_mds, gower = gower_interpolation_mds, fast = fast_mds)
