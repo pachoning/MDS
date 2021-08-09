8eb7350f29fb0931d2500347fe84fed04378d99e
cristianpachon <cc.pachon@gmail.com>
2021-08-09 14:19:05 +0200

    Use dist_fn in gower_mds_main function

--- final_methods/gower_interpolation_mds.R
+++ final_methods/gower_interpolation_mds.R
@@ -2,16 +2,16 @@ source("final_methods/classical_mds.R")
 source("tools/procrustes.R")
 
 get_partitions_for_gower_interpolation <- function(n, n_obs, l, k) {
-
+  
   if (l<=k) {
     stop("l must be greater than k")
   }
-
+  
   p <- 1 + ceiling((n - l)/n_obs)
   n_last <- n - (l + (p-2)*n_obs)
-
+  
   permutation <- sample(x = n, size = n, replace = FALSE)
-
+  
   if (p>1) {
     first_part <- permutation[1:l]
     middle_part <- permutation[(l+1):(n-n_last)]
@@ -22,7 +22,7 @@ get_partitions_for_gower_interpolation <- function(n, n_obs, l, k) {
     list_index[[p-1]] <- list_index[[1]]
     list_index[[1]] <- first_part
     list_index[[p]] <- last_part
-
+    
   } else {
     list_index <- list(permutation)
   }
@@ -36,17 +36,21 @@ get_P_matrix <- function(n_row) {
   one_vector <- matrix(data = 1, nrow = n_row, ncol = 1)
   P <- identity_matrix - 1/n_row * one_vector %*% t(one_vector)
   return(P)
-
+  
 }
 
-gower_mds_main <- function(idx, x, data_1, x_1, n_row_1, q_vector, x_1__s_1__inv) {
-
+gower_mds_main <- function(idx, x, data_1, x_1, n_row_1, q_vector, x_1__s_1__inv, dist_fn, ...) {
+  
   # Filter the matrix
   x_other <- x[idx, , drop = FALSE]
   n_row_other <- nrow(x_other)
+  n_row_1 <- nrow(data_1)
   
   # Get A matrix
-  A <- as.matrix(pdist::pdist(x_other, data_1))
+  full_A <- dist_fn(x = rbind(x_other, data_1), ...)
+  full_A <- as.matrix(full_A)
+  n_col_A <- ncol(full_A)
+  A <- full_A[1:n_row_other, (n_col_A-n_row_1+1):n_col_A, drop = FALSE]
   
   # Get delta matrix
   A_sq <- A^2
@@ -60,8 +64,8 @@ gower_mds_main <- function(idx, x, data_1, x_1, n_row_1, q_vector, x_1__s_1__inv
   return(x_2)
 }
 
-gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist, n_cores = 1, ...) {
-
+gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist, n_cores = 1,...) {
+  
   n <- nrow(x)
   n_row_partition <- l
   indexes_partition <- get_partitions_for_gower_interpolation(n = n, n_obs = n_row_partition, l = l, k = k)
@@ -82,7 +86,7 @@ gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist, n_cores = 1,
     
     # Get the first group 
     n_row_1 <- length(indexes_partition[[1]])
-
+    
     # Obtain MDS for the first group
     data_1 <- x[indexes_partition[[1]], ,drop = FALSE]
     #mds_eig <- classical_mds(x = data_1, k = k, dist_fn = dist_fn, return_distance_matrix = TRUE, ...)
@@ -98,7 +102,7 @@ gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist, n_cores = 1,
     q_vector <- diag(Q)
     S <- 1 / (n_row_1-1) * t(X_1) %*% X_1
     x_1__s_1__inv <- X_1 %*% solve(S)
-
+    
     # Calculations needed to do Gower interpolation
     mds_others <- parallel::mclapply(indexes_partition[2:num_partitions],
                                      gower_mds_main, 
@@ -108,10 +112,23 @@ gower_interpolation_mds <- function(x, l, k, dist_fn = stats::dist, n_cores = 1,
                                      n_row_1 = n_row_1, 
                                      q_vector = q_vector,
                                      x_1__s_1__inv = x_1__s_1__inv,
+                                     dist_fn = dist_fn,
                                      mc.cores = n_cores)
-    
-    mds_points <- do.call(rbind, mds_others)
-    mds_points <- rbind(X_1, mds_points)
+    #mds_others <- parallel::mclapply(indexes_partition[2:num_partitions],
+    #                                 gower_mds_main, 
+    #                                 x = x, 
+    #                                 data_1 = data_1,
+    #                                 x_1 = X_1, 
+    #                                 n_row_1 = n_row_1, 
+    #                                 q_vector = q_vector,
+    #                                 x_1__s_1__inv = x_1__s_1__inv,
+    #                                 dist_fn = dist_fn,
+    #                                 mc.cores = n_cores,
+    #                                 ...)
+    #
+    mds_points <- matrix(data = NA, nrow = n, ncol = k)
+    mds_points[1:n_row_1, ] <- X_1
+    mds_points[(n_row_1+1):n, ] <- do.call(rbind, mds_others)
     idx_all <- do.call(c, indexes_partition)
     idx_all <- order(idx_all)
     mds_points <- mds_points[idx_all, , drop = FALSE]
