f02f6e34f1a667e177fb4b34d34cb8823ea09067
cristianpachon <cc.pachon@gmail.com>
2021-10-21 12:20:53 +0200

    Add geom_points() to memory and distance plots

--- scripts/analysis/cmdscale/plot_time_memory.R
+++ scripts/analysis/cmdscale/plot_time_memory.R
@@ -6,6 +6,7 @@ df_info_process_filter %>%
   ggplot(aes(x = sample_size, y = time_mds)) +
   theme(panel.spacing.y=unit(0.5, "lines"), plot.title = element_text(hjust = 0.5)) +
   geom_line() +
+  geom_point() +
   xlab("Sample Size") +
   ylab("Time (sec.)") +
   ggsave(file.path(getwd(), "images", "time_cmdscale.png"))
@@ -15,6 +16,7 @@ df_info_process_filter %>%
   ggplot(aes(x = sample_size, y = memory_distance)) +
   theme(panel.spacing.y=unit(0.5, "lines"), plot.title = element_text(hjust = 0.5)) +
   geom_line() +
+  geom_point() +
   xlab("Sample Size") +
   ylab("Memoey (MB)") +
   ggsave(file.path(getwd(), "images", "memory_distance.png"))
