ee5586c4ef97a8543daf995e7b3cf54919fdc484
cristianpachon <cc.pachon@gmail.com>
2021-10-22 07:37:23 +0200

    Resize images for time and memory needed for cmdscale

--- scripts/analysis/cmdscale/plot_time_memory.R
+++ scripts/analysis/cmdscale/plot_time_memory.R
@@ -5,7 +5,6 @@ df_info_process_filter[c(94, 95), 'time_mds'] <- c(850, 860)
 df_info_process_filter %>% 
   ggplot(aes(x = sample_size, y = time_mds)) +
   theme(panel.spacing.y=unit(0.5, "lines"), plot.title = element_text(hjust = 0.5)) +
-  geom_line() +
   geom_point() +
   xlab("Sample Size") +
   ylab("Time (sec.)") +
@@ -15,7 +14,6 @@ df_info_process_filter %>%
   mutate(memory_distance = memory_distance/1000000) %>% 
   ggplot(aes(x = sample_size, y = memory_distance)) +
   theme(panel.spacing.y=unit(0.5, "lines"), plot.title = element_text(hjust = 0.5)) +
-  geom_line() +
   geom_point() +
   xlab("Sample Size") +
   ylab("Memoey (MB)") +
