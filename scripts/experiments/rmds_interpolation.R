fbde820816bae0d9023868d59d1448e824c6fc02
cristianpachon <cc.pachon@gmail.com>
2022-07-20 16:35:53 +0200

    Add more experiments

---
+++ scripts/experiments/rmds_interpolation.R
@@ -0,0 +1,47 @@
+#setwd("~/MEGA_tesis_cristian/mds_for_big_data/techincal/MDS")
+source("tools/simulator.R")
+source("final_methods/gower_interpolation_mds.R")
+source("final_methods/emmanuel_mds.R")
+
+J <- 7
+
+for (j in 1:J) {
+  results_folder_name <- paste0("rmds_interpolation_", j)
+  experiment_label = "rmds_interpolation"
+  
+  sample_size = c(5*10^5, 10^6)
+  n_cols = c(10)
+  
+  distribution_parameters <- list(list(var = rep(15, 5)))
+  
+  scenarios <- list(sample_size = sample_size, n_cols = n_cols, distribution_parameters = distribution_parameters)
+  algorithms <- list(gower = gower_interpolation_mds, emmanuel = emmanuel_mds)
+  
+  path <- file.path(getwd(), "data", "experiments", results_folder_name)
+  n_simulations <- 1
+  overwrite_simulations <- TRUE
+  n_sampling_points <- NA
+  l_emmanuel <- 1000
+  l_gower <- 1000
+  n_cores <- 5 #5
+  num_mds_dimesions <- NA
+  verbose <- TRUE
+  
+  get_simulations(
+    experiment_label = experiment_label, 
+    scenarios = scenarios,
+    path = path,
+    algorithms = algorithms,
+    n_simulations = n_simulations,
+    overwrite_simulations = overwrite_simulations,
+    n_sampling_points = n_sampling_points,
+    l_gower = l_gower,
+    l_emmanuel = l_emmanuel,
+    n_cores = n_cores,
+    num_mds_dimesions = num_mds_dimesions,
+    verbose = verbose
+  )
+  
+}
+
+
