c57c0d882838871748fdf45a57c4499806ac4364
pachoning <cc.pachon@gmail.com>
2024-01-09 16:34:48 +0100

    Delete unnecessary scripts

--- scripts/analysis/cmdscale/plot_cmdscale_time.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-load("data/cmdscale/df_cost_time.RData")
-
-df_cost_time %>%
-  ggplot(aes(x = l, y = time, color = event)) +
-  geom_line()
-
-df_cost_time %>% filter(l == 2500)
